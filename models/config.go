3bed985c503d8fe34fc8858f008c4f3464f1ff98
zhangguanping <114462011@qq.com>
2019-04-25 14:00:11 +0800

    添加数据库信息

--- models/config.go
+++ models/config.go
@@ -23,7 +23,7 @@ import (
 
 func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "用户名:密码@/数据库?charset=utf8&loc=Asia%2FShanghai")
+	orm.RegisterDataBase("default", "mysql", "root:zhangxiang@/db_weblog?charset=utf8&loc=Asia%2FShanghai")
 	logs.Info("\n连接数据库成功!")
 }
 
